03f8119aa0f5299806513cc9f006b30467fe6ecb
jet <jiajie8362@gmail.com>
2018-06-20 22:59:56 +0800

    [JJ] chapter 2

--- util.go
+++ util.go
@@ -22,14 +22,14 @@ type Configuration struct {
 var config Configuration
 var logger *log.Logger
 
+// Convenience function for printing to stdout
 func p(a ...interface{}) {
 	fmt.Println(a)
 }
 
 func init() {
 	loadConfig()
-	file, err := os.OpenFile("chitchat.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 6666)
-
+	file, err := os.OpenFile("chitchat.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalln("Failed to open log file", err)
 	}
@@ -49,11 +49,13 @@ func loadConfig() {
 	}
 }
 
+// Convenience function to redirect to the error message page
 func error_message(writer http.ResponseWriter, request *http.Request, msg string) {
 	url := []string{"/err?msg=", msg}
 	http.Redirect(writer, request, strings.Join(url, ""), 302)
 }
 
+// Checks if the user is logged in and has a session, if not err is not nil
 func session(writer http.ResponseWriter, request *http.Request) (sess data.Session, err error) {
 	cookie, err := request.Cookie("_cookie")
 	if err == nil {
@@ -87,6 +89,7 @@ func generateHTML(writer http.ResponseWriter, data interface{}, filenames ...str
 	templates.ExecuteTemplate(writer, "layout", data)
 }
 
+// for logging
 func info(args ...interface{}) {
 	logger.SetPrefix("INFO ")
 	logger.Println(args...)
@@ -96,11 +99,11 @@ func danger(args ...interface{}) {
 	logger.SetPrefix("ERROR ")
 	logger.Println(args...)
 }
+
 func warning(args ...interface{}) {
 	logger.SetPrefix("WARNING ")
 	logger.Println(args...)
 }
-
 
 // version
 func version() string {
